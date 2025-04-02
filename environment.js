e4063486dddec4c59a7de24511db35f377489855
Andrey <70536615+ahdpe@users.noreply.github.com>
2025-04-02 22:32:55 +0200

    Update environment.js

--- environment.js
+++ environment.js
@@ -1,23 +1,15 @@
 // environment.js: Отвечает за создание окружения
 
 function createEnvironment(scene) {
-    // Создаем свет
     var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
 
-    // Создаем землю
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 50, height: 50}, scene); // Сделал землю побольше
-
-    // Создаем и применяем материал земли
+    var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 50, height: 50}, scene);
     var groundMaterial = new BABYLON.StandardMaterial("groundMat", scene);
-    groundMaterial.diffuseColor = new BABYLON.Color3(0, 0.6, 0); // Зеленый цвет
-    // groundMaterial.wireframe = true; // Раскомментируйте для отладки, чтобы видеть сетку
+    groundMaterial.diffuseColor = new BABYLON.Color3(0, 0.6, 0);
     ground.material = groundMaterial;
-
-    // Включаем проверку столкновений для земли (может понадобиться позже)
     ground.checkCollisions = true;
 
-    // Возвращаем созданные объекты
     return {
         ground: ground
     };
